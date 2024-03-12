5e93f02453eddcdc95a4c72d64ed9966902dbe17
Vityambik <vityambik13@gmail.com>
2024-03-13 02:09:01 +0600

    tests for profileRestController

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,103 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.javarush.jira.common.util.JsonUtil.writeValue;
+import static com.javarush.jira.login.internal.web.UserTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileRestController.REST_URL;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.USER_ID;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 class ProfileRestControllerTest extends AbstractControllerTest {
 
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getUser() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(USER_PROFILE_TO));
+    }
+
+    @Test
+    @WithUserDetails(value = GUEST_MAIL)
+    void getGuest() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(GUEST_PROFILE_EMPTY_TO));
+    }
+
+    @Test
+    void getUnauthorized() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateSuccess() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(USER_PROFILE_TO));
+
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(writeValue(getUpdatedTo(USER_ID))))
+                .andExpect(status().isNoContent());
+
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(getUpdatedTo(USER_ID)));
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateInvalid() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(writeValue(getInvalidTo(USER_ID))))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateUnknownNotification() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(writeValue(getWithUnknownNotificationTo(USER_ID))))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateUnknownContact() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(writeValue(getWithUnknownContactTo(USER_ID))))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateContactHtmlUnsafe() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(writeValue(getWithContactHtmlUnsafeTo(USER_ID))))
+                .andExpect(status().isUnprocessableEntity());
+    }
 }
--- src/test/java/com/javarush/jira/profile/internal/web/ProfileTestData.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileTestData.java
@@ -3,41 +3,29 @@ package com.javarush.jira.profile.internal.web;
 import com.javarush.jira.MatcherFactory;
 import com.javarush.jira.profile.ContactTo;
 import com.javarush.jira.profile.ProfileTo;
-import com.javarush.jira.profile.internal.model.Contact;
-import com.javarush.jira.profile.internal.model.Profile;
 
 import java.util.Collections;
 import java.util.Set;
 
 public class ProfileTestData {
-    public static MatcherFactory.Matcher<Profile> PROFILE_MATCHER =
-            MatcherFactory.usingIgnoringFieldsComparator(Profile.class, "user");
 
-    public static ProfileTo USER_PROFILE_TO = new ProfileTo(null,
+    public static MatcherFactory.Matcher<ProfileTo> PROFILE_TO_MATCHER =
+            MatcherFactory.usingIgnoringFieldsComparator(ProfileTo.class);
+
+    public static final long USER_ID = 1L;
+    public static final long GUEST_ID = 3L;
+
+    public static ProfileTo USER_PROFILE_TO = new ProfileTo(USER_ID,
             Set.of("assigned", "overdue", "deadline"),
             Set.of(new ContactTo("skype", "userSkype"),
                     new ContactTo("mobile", "+01234567890"),
                     new ContactTo("website", "user.com")));
-    public static ProfileTo GUEST_PROFILE_EMPTY_TO = new ProfileTo(null,
+    public static ProfileTo GUEST_PROFILE_EMPTY_TO = new ProfileTo(GUEST_ID,
             Set.of(),
             Set.of());
 
-    public static ProfileTo getNewTo() {
-        return new ProfileTo(null,
-                Set.of("three_days_before_deadline", "two_days_before_deadline", "one_day_before_deadline"),
-                Set.of(new ContactTo("tg", "guestTg")));
-    }
-
-    public static Profile getNew(long id) {
-        Profile profile = new Profile();
-        profile.setId(id);
-        profile.setMailNotifications(14);
-        profile.setContacts(Set.of(new Contact(id, "tg", "guestTg")));
-        return profile;
-    }
-
-    public static ProfileTo getUpdatedTo() {
-        return new ProfileTo(null,
+    public static ProfileTo getUpdatedTo(long id) {
+        return new ProfileTo(id,
                 Set.of("assigned", "three_days_before_deadline", "two_days_before_deadline", "one_day_before_deadline", "deadline", "overdue"),
                 Set.of(new ContactTo("skype", "newSkype"),
                         new ContactTo("mobile", "+380987654321"),
@@ -48,40 +36,26 @@ public class ProfileTestData {
                         new ContactTo("linkedin", "newLinkedin")));
     }
 
-    public static Profile getUpdated(long id) {
-        Profile profile = new Profile();
-        profile.setId(id);
-        profile.setMailNotifications(63);
-        profile.setContacts(Set.of(new Contact(id, "skype", "newSkype"),
-                new Contact(id, "mobile", "+380987654321"),
-                new Contact(id, "website", "new.com"),
-                new Contact(id, "github", "newGitHub"),
-                new Contact(id, "tg", "newTg"),
-                new Contact(id, "vk", "newVk"),
-                new Contact(id, "linkedin", "newLinkedin")));
-        return profile;
-    }
-
-    public static ProfileTo getInvalidTo() {
-        return new ProfileTo(null,
+    public static ProfileTo getInvalidTo(long id) {
+        return new ProfileTo(id,
                 Set.of(""),
                 Set.of(new ContactTo("skype", "")));
     }
 
-    public static ProfileTo getWithUnknownNotificationTo() {
-        return new ProfileTo(null,
+    public static ProfileTo getWithUnknownNotificationTo(long id) {
+        return new ProfileTo(id,
                 Set.of("WrongNotification"),
                 Collections.emptySet());
     }
 
-    public static ProfileTo getWithUnknownContactTo() {
-        return new ProfileTo(null,
+    public static ProfileTo getWithUnknownContactTo(long id) {
+        return new ProfileTo(id,
                 Collections.emptySet(),
                 Set.of(new ContactTo("WrongContactCode", "contact")));
     }
 
-    public static ProfileTo getWithContactHtmlUnsafeTo() {
-        return new ProfileTo(null,
+    public static ProfileTo getWithContactHtmlUnsafeTo(long id) {
+        return new ProfileTo(id,
                 Collections.emptySet(),
                 Set.of(new ContactTo("tg", "<script>alert(123)</script>")));
     }
